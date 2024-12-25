61a1683eb952057dd25c432e9fa6aa82bb03c703
jaat301 <68196478+jaat301@users.noreply.github.com>
2024-12-25 16:54:11 +0530

    Update new.php

--- new.php
+++ new.php
@@ -1,8 +1,3 @@
 <?php
-$extensions = get_loaded_extensions();
-if (in_array('posix', $extensions)) {
-    echo 'POSIX extension is loaded.';
-} else {
     echo 'POSIX extension is not loaded.';
-}
 ?>
